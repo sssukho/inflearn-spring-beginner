31d05091bd93f4946278e6ed38aa102a6da29441
sssukho <sssukho@gmail.com>
2022-11-01 00:00:46 +0900

    프로젝트 세팅

---
+++ core/src/main/java/hello/core/SpringBeginnerApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeginnerApplication.class, args);
+	}
+
+}
