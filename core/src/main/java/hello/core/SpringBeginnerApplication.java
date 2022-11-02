d28048d78632e4e19bbeab4d69e65b5dcd19a4bf
sssukho <sssukho@gmail.com>
2022-11-02 22:13:51 +0900

    main class 수정

--- core/src/main/java/hello/core/SpringBeginnerApplication.java
+++ core/src/main/java/hello/core/SpringBeginnerApplication.java
@@ -1,21 +1,12 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringBeginnerApplication {
-
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringBeginnerApplication.class, args);
-		MemberService memberService = new MemberServiceImpl();
-		Member member = new Member(1L, "memberA", Grade.VIP);
-		memberService.join(member);
-
-		Member findMember = memberService.findMember(1L);
-		System.out.println("new member = " + member.getName());
-		System.out.println("find Member = " + findMember.getName());
+		SpringApplication.run(SpringBeginnerApplication.class, args);
 	}
 
 }
