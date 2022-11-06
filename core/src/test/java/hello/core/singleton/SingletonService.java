1de71ec33147919ffab676da1fff41d034610ffd
sssukho <sssukho@gmail.com>
2022-11-06 22:10:31 +0900

    섹션 5. 싱글톤 컨테이너 - 싱글톤 방식의 주의점 내용 추가

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -2,8 +2,12 @@ package hello.core.singleton;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonService {
 
